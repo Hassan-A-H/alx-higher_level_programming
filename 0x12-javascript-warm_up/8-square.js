8f33c0c0bc596f062738a6d40eb11b366f321e5c
Hassan-A-H <h23798a@gmail.com>
2024-06-24 16:43:57 +0200

    a script that prints a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+const num = Number(argv[2]);
+
+if (Number.isNaN(num)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < num; i++) {
+    let row = '';
+    for (let j = 0; j < num; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+}
