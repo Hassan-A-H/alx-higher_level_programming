3a9e40ad872d33d5ba78a87a37f83babf39f8174
Hassan-A-H <h23798a@gmail.com>
2024-06-29 18:46:20 +0200

    edit style

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -10,9 +10,9 @@ module.exports = class Rectangle {
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      let rec = "";
+      let rec = '';
       for (let j = 0; j < this.width; j++) {
-	rec += "X";
+        rec += 'X';
       }
       console.log(rec);
     }
